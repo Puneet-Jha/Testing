f4af4dccb2d32d65db4bee6c2fc13355a82f6230
Puneet-Jha <139774590+Puneet-Jha@users.noreply.github.com>
2026-09-09 22:55:29 +0530

    Update 1.2 prob.py

--- 1.2 prob.py
+++ 1.2 prob.py
@@ -1,19 +1,21 @@
 # Distance threshold 
-OBSTACLE_DISTANCE = 20 
+SAFE_DISTANCE = 20 
 
 while True:
     distance = get_distance()
+    #get_distance = input quantity from sensors
   
     if distance > OBSTACLE_DISTANCE:
 
+        # Following functions are to move the motor forward
         left_motor_forward()
         right_motor_forward()
 
     else:
 
+        # Following functions are to move the motor in the desired manner
         left_motor_stop()
         right_motor_stop()
-
 
         left_motor_forward()
         right_motor_backward()
